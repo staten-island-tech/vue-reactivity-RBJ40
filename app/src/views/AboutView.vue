a7ca5ad9f96da49fc82df6ef2e8574e6af9c896d
RBJ40 <celinechan268@gmail.com>
2025-02-04 12:29:35 -0500

    draft

--- app/src/views/AboutView.vue
+++ app/src/views/AboutView.vue
@@ -7,14 +7,29 @@
   </div>
 </template>
 
-<!-- might need to put this in javascript file
+<!-- might need to put this in javascript file. might also add categories for each product
 <script>
 const groceryItems = [
   { name: 'Apple', price: '$1.25' },
   { name: 'Orange', price: '$1.25' },
-  { name: 'Kiwi', price: '$5' },
+  { name: 'Kiwi', price: '$1.25' },
   { name: 'Pear', price: '$1.25'},
   { name: 'Peach', price: '$1.25'},
+  { name: 'Banana', price: '$1.25'},
+  { name: 'Strawberry', price: '$3'},
+  { name: 'Grape', price: '$2'},
+  { name: 'Cherry', price: '$3'},
+  { name: 'Durian', price: '$1.50'},
+  { name: 'Cranberry', price: '$2'},
+  { name: 'Blueberry', price: '$3'},
+  { name: 'Pork', price: '$5'},
+  { name: 'Chicken drumstick', price: '$5'},
+  { name: 'Chicken wing', price: '$5'},
+  { name: 'Beef', price: '$5'},
+  { name: 'Turkey', price: '$5'},
+  { name: 'Duck', price: '$5'},
+  { name: 'Sausage', price: '$4'}
+  { name: 'Fish', price: '$6'}
 ]
 </script> -->
 
