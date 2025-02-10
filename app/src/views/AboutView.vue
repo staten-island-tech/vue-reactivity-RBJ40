2c70c9bb8ce179c6812b2e94f6af06a41a250ad3
RBJ40 <celinechan268@gmail.com>
2025-02-10 12:29:46 -0500

    draft
    might need to add more vue files in icons folder

--- app/src/views/AboutView.vue
+++ app/src/views/AboutView.vue
@@ -2,13 +2,13 @@
   <div class="about">
     <h1>Grocery shop</h1>
     <p>Buy</p>
-    <h1 v-for="groceryItem in groceryItems">{{ groceryItem.name }}</h1>
-    <h2 v-for="groceryItem in groceryItems">{{ groceryItem.price }}</h2>
+    <h1 v-for="groceryItem in groceryItems" :key="groceryItem">{{ groceryItem.name }}</h1>
+    <h2 v-for="groceryItem in groceryItems" :key="groceryItem">{{ groceryItem.price }}</h2>
   </div>
 </template>
 
 <script setup>
-import groceryItems from './views/list.js'
+import groceryItems from '../views/list.js'
 </script>
 
 <style>
