4ee10a2404c63e067a3603453f20bf44974ca290
RBJ40 <celinechan268@gmail.com>
2025-02-24 21:56:52 -0500

    Update ShoppingCart.vue

--- app/src/components/ShoppingCart.vue
+++ app/src/components/ShoppingCart.vue
@@ -1,1 +1,13 @@
+<script setup>
+import groceryItems from '@/components/GroceryItems.vue'
+import { groceryItems } from './list'
+</script>
 
+<template>
+    <div>
+        <button class="quantity-add" @click="add(groceryItem)">+</button>
+      <h3 class="quantity-count">{{ groceryItem.quantity }}</h3>
+      <button class="quantity-subtract" @click="subtract(merch)">-</button>
+
+    </div>
+</template>
