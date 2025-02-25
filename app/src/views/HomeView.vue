c61eb845955afd25fdc87063a3d7a41648963ee9
RBJ40 <celinechan268@gmail.com>
2025-02-24 22:19:25 -0500

    Update HomeView.vue

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,21 +1,15 @@
 <script setup>
-import { groceryItems } from './components/GroceryItems.vue'
+import groceryItems from '@/components/GroceryItems.vue'
+import { groceryItems } from './list';
 </script>
 
 <template>
   <main>
     <h1>Grocery shop</h1>
     <p>Buy</p>
-    <div v-for="groceryItem in groceryItems" :key="groceryItem">
-      <h1>{{ groceryItem.name }}</h1>
-      <h2>{{ groceryItem.price }}</h2>
-      <img :src="groceryItem.img" />
-      <!--might add imgDesc here-->
-      <!-- <button @click="$emit"('AddtoCart', index)">Add to Cart</button>
-      <slot></slot> -->
-    <div class="container">
-      <div class="card"><GroceryItems/></div>
-    </div>
+    <div v-for="item in groceryItems" :key="item.name">
+      <h2>{{ item.name }} - {{ item.price }}</h2>
+      <img :src="item.img" alt="Grocery Item" />
     </div>
   </main>
 </template>
