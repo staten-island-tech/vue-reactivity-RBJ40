b25d926f6619a388c451ee91eceba908778f2201
RBJ40 <celinechan268@gmail.com>
2025-02-25 22:51:01 -0500

    Update AboutView.vue

--- app/src/views/AboutView.vue
+++ app/src/views/AboutView.vue
@@ -1,19 +1,5 @@
-<!-- <template>
-  <div class="about">
-  </div>
-</template>
+<template></template>
 
-<script setup>
+<script setup></script>
 
-</script>
-
-<style scoped>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
- -->
+<style></style>
