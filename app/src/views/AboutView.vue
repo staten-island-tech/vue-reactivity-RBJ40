f47ec741009fca042270d60ed7c505dac777f219
RBJ40 <celinechan268@gmail.com>
2025-02-25 22:48:07 -0500

    Update AboutView.vue

--- app/src/views/AboutView.vue
+++ app/src/views/AboutView.vue
@@ -1,14 +1,5 @@
-<template>
+<!-- <template>
   <div class="about">
-    <h1>Grocery shop</h1>
-    <p>Buy</p>
-    <div v-for="groceryItem in groceryItems" :key="groceryItem">
-      <h1>{{ groceryItem.name }}</h1>
-      <h2>{{ groceryItem.price }}</h2>
-      <img :src="groceryItem.img" />
-      <!--might add imgDesc here-->
-      <slot></slot>
-    </div>
   </div>
 </template>
 
@@ -25,3 +16,4 @@
   }
 }
 </style>
+ -->
