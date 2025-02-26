a61be5c4da1f1bca023f4d82e670140d24922b6e
RBJ40 <celinechan268@gmail.com>
2025-02-25 23:17:12 -0500

    Update ShoppingCart.vue

--- app/src/components/ShoppingCart.vue
+++ app/src/components/ShoppingCart.vue
@@ -15,7 +15,6 @@ defineProps({
 <template>
   <div class="cart">
     <h2>Shopping Cart:</h2>
-    <h2>-----------------</h2>
     <ul>
       <li v-for="(item, index) in cart" :key="index">
         <h2>{{ item.name }} - ${{ item.price }}</h2>
