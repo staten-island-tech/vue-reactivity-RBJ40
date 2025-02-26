4055d6f4a5a311d530a6c98941ad3b44a5b50281
RBJ40 <celinechan268@gmail.com>
2025-02-25 22:41:31 -0500

    Update ShoppingCart.vue

--- app/src/components/ShoppingCart.vue
+++ app/src/components/ShoppingCart.vue
@@ -1,13 +1,37 @@
 <script setup>
-import groceryItems from '@/components/GroceryItems.vue'
-import { groceryItems } from './list'
+import { ref } from 'vue'
+const price = ref('$0.00'); //starting price = $0.00
+
+const deleteItem = (index) => {
+  console.log(index, 'Delete item function')
+  item.value.splice(index, 1);
+}
+
+defineProps({
+  cart: Array, 
+})
 </script>
 
 <template>
-    <div>
-        <button class="quantity-add" @click="add(groceryItem)">+</button>
-      <h3 class="quantity-count">{{ groceryItem.quantity }}</h3>
-      <button class="quantity-subtract" @click="subtract(merch)">-</button>
-
-    </div>
+  <div class="cart">
+    <h2>Shopping Cart:</h2>
+    <h2>-----------------</h2>
+    <ul>
+      <li v-for="(item, index) in cart" :key="index">
+        <h2>{{ item.name }} - ${{ item.price }}</h2>
+        <button @click="deleteItem(index)">Remove Item</button>
+      </li>
+    </ul>
+    <h3>Total Cost: ${{ price }}</h3>
+  </div>
 </template>
+
+
+<style lang="css" scoped>
+.cart {
+  height: fit-content;
+  width: 25%;
+  border: 5px solid black;
+}
+
+</style>
