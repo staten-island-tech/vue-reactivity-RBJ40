9dcc368598301256b4b37f4747781bdff517f590
RBJ40 <celinechan268@gmail.com>
2025-02-25 21:59:13 -0500

    Create GroceryCard.vue

---
+++ app/src/components/GroceryCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="card">
+    <h2>{{ name }}</h2>
+    <h3>Price: ${{ price }}</h3>
+    <img :src="img" alt="Grocery Item"/>
+    <button @click="$emit('AddtoCart', index)">Add to Cart</button>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="css" scoped>
+.card{
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  border: 8px solid red;
+}
+img{
+    object-fit: contain;
+}
+</style>
