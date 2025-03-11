107ac45ff10644d77fd4108d257bbad9bd89e4a9
RBJ40 <celinechan268@gmail.com>
2025-03-10 23:22:55 -0400

    Update App.vue

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,15 +1,3 @@
-<script setup>
-import { groceryItems } from '@/components/GroceryItems.vue';
-</script>
-,
 <template>
-  <main>
-    <h1>Grocery shop</h1>
-    <p>Buy</p>
-    <div v-for="item in groceryItems" :key="item.name">
-      <h2>{{ item.name }} - {{ item.price }}</h2>
-      <img :src="item.img" alt="Grocery Item" />
-    </div>
-  </main>
+  <div><RouterView /></div>
 </template>
-
